cc971204d9de287720237854e2449def9d4e77da
Jay Malli <mallijay747@gmail.com>
2023-10-19 08:59:33 +0530

    initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+namespace logs;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		throw new Exception("Counter Button Clicked!");
+	}
+
+	private void OnGetFilesClicked(object sender, EventArgs e)
+	{
+		try
+		{
+			string path = "/demo_logs";
+			Directory.GetFiles(path);
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message);
+		}
+	}
+
+	private void CurrentDomain_FirstChanceException(object sender, FirstChanceExceptionEventArgs e)
+	{
+		if (e.Exception is Exception ex)
+		{
+			LogException(ex);
+		}
+	}
+
+	private void LogException(Exception ex)
+	{
+		string logsFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "app_logs.txt");
+		try
+		{
+			StackFrame sf = new StackFrame(1);
+			MethodBase method = sf.GetMethod();
+			int line = sf.GetFileLineNumber();
+			string exMsg = Environment.CurrentManagedThreadId + " ~ " + DateTime.Now.ToString() + method.DeclaringType.FullName + "::" + line + "-" + GetErrorMessage(ex);
+			File.AppendAllText(logsFilePath, exMsg + Environment.NewLine + Environment.NewLine);
+		}
+		catch (Exception exe)
+		{
+			Console.WriteLine(exe.Message);
+		}
+	}
+
+	private string GetErrorMessage(Exception ex)
+	{
+		if (ex != null)
+		{
+			string message = ex.StackTrace + " " + ex.Message;
+			if (ex.InnerException != null)
+			{
+				message += "~ Caused By" + GetErrorMessage(ex.InnerException);
+			}
+			return message;
+		}
+
+		return string.Empty;
+	}
+
+}
+
---
+++ ReadMe.md
@@ -0,0 +1,10 @@
+# Implement Logging Service in .NET MAUI
+
+- It's Contains code for how to implement Logging service in .NET MAUI without using any external Library/Packages.
+
+- Checkout [My Blog](https://dev.to/jaymalli_programmer/extract-content-from-odf-files-using-c-132b) for better understanding of this code.
+
+### Requirements
+
+- dotnet 7.0 SDK
+- visual studio code / visual studio
